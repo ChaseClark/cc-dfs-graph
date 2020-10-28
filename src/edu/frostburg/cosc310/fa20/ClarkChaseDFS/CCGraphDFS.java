789437902f1f1007013f3fbf3f4b620409013256
Chase J. Clark <chase_clark07@hotmail.com>
2020-10-28 15:26:53 -0400

    File input working

--- src/edu/frostburg/cosc310/fa20/ClarkChaseDFS/CCGraphDFS.java
+++ src/edu/frostburg/cosc310/fa20/ClarkChaseDFS/CCGraphDFS.java
@@ -2,40 +2,96 @@ package edu.frostburg.cosc310.fa20.ClarkChaseDFS;
 
 import edu.frostburg.cosc310.fa20.COSC310_P01;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class CCGraphDFS implements COSC310_P01 {
 
-    int[][] adjMatrix = createAdjMatrix("worldmap_0000.txt",26);
+    private int[][] adjMatrix;
 
-    private int[][] createAdjMatrix(String input, int arrayLength) {
+    private int[][] createAdjMatrix(String path, int arrayLength) {
         var matrix = new int[arrayLength][arrayLength];
 
         // read file from disk
-
-
-
-
+        try {
+            List<String> allLines = Files.readAllLines(Path.of(path));
+            for (String line: allLines) {
+                if (!line.startsWith("#")) // these lines are comments
+                {
+                    String[] lineArr = line.split(" "); // each line is space-delimited
+                    if (lineArr.length > 0) {// check if line has values
+                        // here we need to set the first entry to be the target index/current node (linArr[0])
+                        System.out.print("createAdjMatrix -> " + lineArr[0] + "-> length: " + lineArr.length);
+                        // convert letter to index integer
+                        var currentNode = convertLetterToIndex(lineArr[0]);
+                        // after this point the values we are looking for are in pairs (connectedNode, cost)
+                        // we need to make a loop that works in pairs could use mod 2 or increment index by 2 each time
+                    }
+                }
+            }
+            System.out.println("=== ARMED AND READY ===");
+        } catch (IOException e) {
+            System.out.println("error finding file at: "+ path);
+            e.printStackTrace();
+        }
         return matrix;
     }
 
     /**
      * Convert a letter to index for our matrix.
      * Get the ASCII code and subtract 65 (uppercase)
-     * @param c char letter
+     * @param c input is a string from a text file but we only want to work with letter characters
      * @return an index where A = 0 and Z = 25
      */
-    private int convertLetterToIndex(char c)
+    private int convertLetterToIndex(String c)
     {
-        return (int)Character.toUpperCase(c) - 65;
+        if (c.length() == 1) { // if a single character
+            return (int)c.toUpperCase().charAt(0) - 65;
+        }
+        else
+            return -1; // fail
     }
 
     @Override
     public void go() {
+        System.out.println("              .andAHHAbnn.\n" +
+                "           .aAHHHAAUUAAHHHAn.\n" +
+                "          dHP^~\"        \"~^THb.\n" +
+                "    .   .AHF                YHA.   .\n" +
+                "    |  .AHHb.              .dHHA.  |\n" +
+                "    |  HHAUAAHAbn      adAHAAUAHA  |\n" +
+                "    I  HF~\"_____        ____ ]HHH  I\n" +
+                "   HHI HAPK\"\"~^YUHb  dAHHHHHHHHHH IHH\n" +
+                "   HHI HHHD> .andHH  HHUUP^~YHHHH IHH\n" +
+                "   YUI ]HHP     \"~Y  P~\"     THH[ IUP\n" +
+                "    \"  `HK                   ]HH'  \"\n" +
+                "        THAn.  .d.aAAn.b.  .dHHP\n" +
+                "        ]HHHHAAUP\" ~~ \"YUAAHHHH[\n" +
+                "        `HHP^~\"  .annn.  \"~^YHH'\n" +
+                "         YHb    ~\" \"\" \"~    dHF\n" +
+                "          \"YAb..abdHHbndbndAP\"\n" +
+                "           THHAAb.  .adAHHF\n" +
+                "            \"UHHHHHHHHHHU\"\n" +
+                "              ]HHUUHHHHHH[\n" +
+                "            .adHHb \"HHHHHbn.\n" +
+                "     ..andAAHHHHHHb.AHHHHHHHAAbnn..\n" +
+                ".ndAAHHHHHHUUHHHHHHHHHHUP^~\"~^YUHHHAAbn.\n" +
+                "  \"~^YUHHP\"   \"~^YUHHUP\"        \"^YUP^\"\n" +
+                "       \"\"         \"~~\"");
+        System.out.println("=== DESTROY ALL HUMANS ===");
         System.out.println("Program starting...");
         System.out.println("Created by: " + getMyName());
 
+
+        // load a test file
         System.out.println("Testing...");
-        System.out.println(convertLetterToIndex('A'));
-        System.out.println(convertLetterToIndex('Z'));
+        adjMatrix = createAdjMatrix("worldmap_0000.txt",26);
+        runDFS1();
+
+
+
     }
 
     @Override
@@ -49,7 +105,7 @@ public class CCGraphDFS implements COSC310_P01 {
      */
     @Override
     public void runDFS1() {
-
+        System.out.println("Search 1: ");
     }
 
     /**
@@ -60,6 +116,7 @@ public class CCGraphDFS implements COSC310_P01 {
      */
     @Override
     public void runDFS2() {
+        System.out.println("Search 2: ");
 
     }
 
@@ -70,6 +127,6 @@ public class CCGraphDFS implements COSC310_P01 {
      */
     @Override
     public void runDFS3() {
-
+        System.out.println("Search 3: ");
     }
 }
