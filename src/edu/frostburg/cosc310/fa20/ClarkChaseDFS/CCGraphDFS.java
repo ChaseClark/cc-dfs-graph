54c63171a26816c423c5deafb0bae71311b0e5d9
Chase J. Clark <chase_clark07@hotmail.com>
2020-10-29 13:47:42 -0400

    DFS2 almost done

--- src/edu/frostburg/cosc310/fa20/ClarkChaseDFS/CCGraphDFS.java
+++ src/edu/frostburg/cosc310/fa20/ClarkChaseDFS/CCGraphDFS.java
@@ -10,13 +10,13 @@ import java.util.List;
 
 public class CCGraphDFS implements COSC310_P01 {
 
-    // TODO: add helper method to print ArrayList
-    // TODO: make dfs stop on X or Y
+    // TODO: Make DFS2 select vertex with lowest edge cost instead of alphabetical
 
     private int[][] graph;
     private ArrayList<Character> visitedNodes = new ArrayList<Character>();
     private boolean targetAcquired = false; // true if X or Y is found when searching
     private int cost; // summation of edge values
+    private boolean giveUp = false; // used for lazyDFS to see if dont searching
 
     private int[][] createAdjMatrix(String path, int arrayLength) {
         var matrix = new int[arrayLength][arrayLength];
@@ -30,7 +30,6 @@ public class CCGraphDFS implements COSC310_P01 {
                     String[] lineArr = line.split(" "); // each line is space-delimited
                     if (lineArr.length > 0) {// check if line has values
                         // here we need to set the first entry to be the target index/current node (linArr[0])
-                        System.out.println("createAdjMatrix -> " + lineArr[0] + "-> length: " + lineArr.length);
                         // convert letter to index integer
                         var currentNode = convertLetterToIndex(lineArr[0]);
                         // after this point the values we are looking for are in pairs (connectedNode, cost)
@@ -120,17 +119,16 @@ public class CCGraphDFS implements COSC310_P01 {
                 "  \"~^YUHHP\"   \"~^YUHHUP\"        \"^YUP^\"\n" +
                 "       \"\"         \"~~\"");
         System.out.println("=== DESTROY ALL HUMANS ===");
+        System.out.println();
         System.out.println("Program starting...");
         System.out.println("Created by: " + getMyName());
+        System.out.println();
 
-
-        // load a test file
-        System.out.println("Testing...");
-        graph = createAdjMatrix("worldmap_0000.txt",26);
+        // load the worldmap
+        graph = createAdjMatrix("worldmap_0002.txt",26);
         runDFS1();
-
-
-
+        runDFS2();
+//        runDFS3();
     }
 
     @Override
@@ -151,18 +149,21 @@ public class CCGraphDFS implements COSC310_P01 {
         visitedNodes.clear();
         // reset cost count
         cost = 0;
-        dfsAlphabetical(0, visited);
+        // reset found = false
+        targetAcquired = false;
+        alphabeticalDFS(0, visited);
         System.out.print("Search 1: Path -> "+visitedNodes.toString()+" Cost -> "+cost+" (");
         // print whether or not target was found
         System.out.println(" "+((targetAcquired) ? "Target Eliminated! Mission Successful! )" : "Target Not Found... Mission Failed... )"));
+        System.out.println();
     }
     // our graph is automatically in alphabetical order due to how our matrix is set up
     // need to count the count until X(23) or Y(24) is found
-    private void dfsAlphabetical(int current, boolean[] visited) {
+    private void alphabeticalDFS(int current, boolean[] visited) {
         // loop through row
         for (int adj = 0; adj< graph.length; adj++) {
             if (graph[current][adj] > 0 && !visited[adj] && !targetAcquired) {
-                System.out.println("Current: "+current+" Letter: "+convertIndexToLetter(adj)+"("+ adj +")"+" ->Cost: "+graph[current][adj]);
+                // System.out.println("Current: "+current+" Letter: "+convertIndexToLetter(adj)+"("+ adj +")"+" ->Cost: "+graph[current][adj]);
                 // mark as visited
                 visited[adj] = true;
                 // add letter to list
@@ -174,7 +175,7 @@ public class CCGraphDFS implements COSC310_P01 {
                     // here we signal to stop searching
                     targetAcquired = true;
                 else
-                    dfsAlphabetical(adj, visited); // keep searching
+                    alphabeticalDFS(adj, visited); // keep searching
             }
         }
     }
@@ -187,8 +188,45 @@ public class CCGraphDFS implements COSC310_P01 {
      */
     @Override
     public void runDFS2() {
-        System.out.println("Search 2: ");
+        var visited = new boolean[graph.length];
+        // reset list
+        visitedNodes.clear();
+        // reset cost count
+        cost = 0;
+        // reset found/giveup to false
+        targetAcquired = giveUp = false;
+        lazyDFS(0,visited,0);
+        System.out.print("Search 2: Path -> "+visitedNodes.toString()+" Cost -> "+cost+" (");
+        // print whether or not target was found
+        System.out.println(" "+((targetAcquired) ? "Target Eliminated! Mission Successful! )" : "Target Not Found... Mission Failed... )"));
+        System.out.println();
+    }
 
+    private void lazyDFS(int current, boolean[] visited, int visitCount) {
+        for (int adj = 0; adj< graph.length; adj++) {
+            if (graph[current][adj] > 0 && graph[current][adj] < 10 && !visited[adj] && !targetAcquired && !giveUp) {
+                System.out.println("Current: "+current+" Letter: "+convertIndexToLetter(adj)+"("+ adj +")"+" ->Cost: "+graph[current][adj]);
+                // mark as visited
+                visited[adj] = true;
+                // add letter to list
+                visitedNodes.add(convertIndexToLetter(adj));
+                // keep track of cost
+                cost += graph[current][adj];
+                visitCount++;
+                // see if we are done
+                if (adj == 23 || adj == 24) // X or Y
+                    // here we signal to stop searching
+                    targetAcquired = true;
+                else if (visitCount >= 5)
+                    giveUp = true;
+                else
+                    lazyDFS(adj, visited, visitCount); // keep searching
+            }
+        }
+    }
+
+    private int selectAdjNodeWithLowestCost(int current){
+        return -1;
     }
 
     /**
@@ -198,7 +236,6 @@ public class CCGraphDFS implements COSC310_P01 {
      */
     @Override
     public void runDFS3() {
-        System.out.println("Search 3: ");
         // use int[] visited and if (visited[adj] < 3)
     }
 }
